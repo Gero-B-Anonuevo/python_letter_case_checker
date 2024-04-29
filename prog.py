8640d98094358926dc5fa8bbf7acbdf94bc032c0
Gero-B-Anonuevo <150780703+Gero-B-Anonuevo@users.noreply.github.com>
2024-04-29 11:52:08 +0800

    Update prog.py

---
+++ prog.py
@@ -0,0 +1,5 @@
+letter = input("Input a letter: ")
+if letter == letter.lower():
+    print("The letter you inputted is in lowercase format")
+elif letter == letter.upper():
+    print("The letter you inserted is in uppercase format")
